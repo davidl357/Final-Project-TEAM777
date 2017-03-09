83e0cf8c0a9ec4df9f084a392a3c3fcd47910094
David Lee <david168@uw.edu>
2017-03-09 11:05:27 -0800

    added map

--- app.R
+++ app.R
@@ -6,6 +6,9 @@ library("httr")
 library("jsonlite")
 library("knitr")
 library("tidyr")
+library("maps")
+library("countrycode")
+library("RColorBrewer")
 
 source("steam api key.R")
 
@@ -20,7 +23,7 @@ get.owned.games <- "IPlayerService/GetOwnedGames/v0001/"
 
 my.ui <- fluidPage(
   
-  tags$style("body {background-image: url(http://fashion.healthdailyheadlines.com/uploads/201411/27/go/gold%20rush%20minimal%20texture%20iphone%206%20plus%20wallpapers%20-%20minimal%20iphone%206%20plus%20wallpapers-f57318.jpg);}"),
+  tags$style("body {background: -webkit-linear-gradient(white, gray);}"),
   
   titlePanel("Statistics About Your Steam Profile"),
 
@@ -51,7 +54,7 @@ my.ui <- fluidPage(
     # data
     sidebarPanel(
       
-      textInput("id", "Input 64 Digit Steam ID", placeholder = "User ID here..."),
+      textInput("id", "Input 64 Digit Steam ID", placeholder = "User ID here...", value = "76561197986603983"),
       textOutput("steamid"),
       
       sliderInput("num.games", "Number of Games to include: (for top games)",
@@ -76,10 +79,6 @@ my.ui <- fluidPage(
 )
 
 my.server <- function(input, output) {
-  
-  output$map <- renderPlot({
-    
-  })
   
   output$top.table <- renderTable({
     
@@ -156,7 +155,7 @@ my.server <- function(input, output) {
     
     message <- paste0("(THIS APPLICATION WILL NOT WORK WITHOUT A USER ID) \n This table shows the top ", num, " played. The graph shows 
                       the top ", num, " played for your library in lifetime hours. The table, shows the top ", num, " games played 
-                      with the name of the game, lifetime hours played, and amount of hours played in the last 2 weeks.")
+                      with the name of the game, lifetime hours played, and amount of hours played in the last 2 weeks. (SAMPLE ID: 76561197986603983)")
     
     return(message)
     
@@ -184,7 +183,7 @@ my.server <- function(input, output) {
   })
 
   # stores user input steam id 
-  output$steamid <- renderText({
+  output$map <- renderPlot({
     
     get.friend.list<-"ISteamUser/GetFriendList/v0001/"
     results<-GET(paste0(base.url,get.friend.list,key,"&steamid=",input$id))
@@ -194,14 +193,11 @@ my.server <- function(input, output) {
     results<-flatten(results)
     is.data.frame(results)
     
-    
     test<-c(TRUE, FALSE, FALSE)
     steam.id.from.friends <- results[test]
     steam.id.from.friends<-gather(steam.id.from.friends)
     collected <- steam.id.from.friends$value[1]
     data.frame(steam.id.from.friends, stringsAsFactors = FALSE)
-    
-    
     
     all.except.one<-steam.id.from.friends$value
     nrow(steam.id.from.friends)
@@ -210,9 +206,40 @@ my.server <- function(input, output) {
     for(id in all.except.one){
       collected<-paste0(collected,",",id)
     }
-    message <- collected
-   
-    return("")
+    print(collected)
+    
+    response.steam <- GET(paste0("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=3651612D23372B3B62F2F3F6DF2B6C76&steamids=",collected))
+    body.steam <- fromJSON(content(response.steam, "text"))
+    steam <- data.frame(body.steam)
+
+    # ISO3
+    steam.friends.iso3 <- data.frame(countrycode(steam$response.players.loccountrycode, "iso2c", "iso3c"))
+    colnames(steam.friends.iso3) <- "FriendISO3"
+    steam.friends.iso3 <- na.omit(steam.friends.iso3)
+    world.data<-map_data("world")
+    world.data$ISO3<-iso.alpha(world.data$region,n=3)
+    unique.iso3 <- unique(select(world.data, ISO3))
+
+    temp <- unique.iso3
+    
+    country.count <- c()
+    
+    for(x in unique.iso3$ISO3)
+    {
+      steam.friends.filtered.iso3 <- filter(steam.friends.iso3, FriendISO3 == x)
+      iso3.count <- nrow(steam.friends.filtered.iso3)
+      country.count[x] <- iso3.count
+    }
+    
+    temp <- mutate(temp, count = country.count)
+    
+    # Map
+    world.data <- left_join(world.data, temp, by = "ISO3")
+    
+    ggplot(data = world.data) +
+      geom_polygon(aes(x = long, y = lat, group = group, fill = cut(count, breaks=c(0:100))))+
+      scale_color_brewer(palette = "Set2") + labs(title = "Number of Friends from Around the World", fill = "Number of Friends")
+      
   })
   
 }
